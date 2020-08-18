7073821705bc93fda903e4b69d6837f60636fd36
Coop <61427314+codelucid@users.noreply.github.com>
2020-08-18 17:53:55 -0400

    creation of Routes files

---
+++ Routes/api/books.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const booksController = require("../../Controllers/booksController");
+
+// Matches with "/api/books"
+router.route("/api/books")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+// Matches with "/api/books/:id"
+router
+  .route("/api/books/:id")
+  .get(booksController.findById)
+  .delete(booksController.remove);
+
+module.exports = router;
