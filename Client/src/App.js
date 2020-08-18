8569705f35e3587f6ff5c891d358a02388dd4bb2
Coop <61427314+codelucid@users.noreply.github.com>
2020-08-18 17:32:07 -0400

    update to app.js file

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./Pages/Search";
+import Saved from "./Pages/Saved";
+
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/api/books"]}>
+            <Search />
+          </Route>
+          <Route exact path={["/api/books/","/api/books/:id"]}>
+            <Saved />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
