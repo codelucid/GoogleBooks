b0a13836e4f0b1f54d34965ab22ec9e18ef237c1
Coop <61427314+codelucid@users.noreply.github.com>
2020-08-21 20:49:41 -0400

    update to book model

--- Models/book.js
+++ Models/book.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  authors: { type: String, default: "" },
-  description: { type: String, default: "" },
-  image: { type: String, default: "" },
-  link: { type: String, default: "", unique: true }
+  authors: [{ type: String, default: "" }],
+  synopsis: { type: String, default: "" },
+  thumbnail: { type: String, default: "" },
+  link: { type: String, default: "" }
 });
 
 const Book = mongoose.model("Book", bookSchema);
