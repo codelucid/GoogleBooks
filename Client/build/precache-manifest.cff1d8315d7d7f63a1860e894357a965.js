26798abdc1061faae71497c4163616f5a4526a5e
Coop <61427314+codelucid@users.noreply.github.com>
2020-08-23 00:00:35 -0400

    deploy to gh-pages

---
+++ Client/build/precache-manifest.cff1d8315d7d7f63a1860e894357a965.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322db25364b792c1b2be6b6912666a1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ab55e319edc2cf4f179",
+    "url": "/static/css/2.fe7cd3c1.chunk.css"
+  },
+  {
+    "revision": "f00200d419fdf7e94045",
+    "url": "/static/css/main.8dedcd70.chunk.css"
+  },
+  {
+    "revision": "7ab55e319edc2cf4f179",
+    "url": "/static/js/2.884839b8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.884839b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00200d419fdf7e94045",
+    "url": "/static/js/main.5c1b0ce2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
