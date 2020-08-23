d901859c3fae4c72fae2a65c168c916c50738a1f
Coop <61427314+codelucid@users.noreply.github.com>
2020-08-23 00:11:10 -0400

    Updates

---
+++ precache-manifest.7ca5ea56668e259c75326a4aa8ef8adf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75cb4bd28c2190e2f8cf3d29296f2716",
+    "url": "/GoogleBooks/index.html"
+  },
+  {
+    "revision": "b9aac28d422c76e4d019",
+    "url": "/GoogleBooks/static/css/2.fe7cd3c1.chunk.css"
+  },
+  {
+    "revision": "43451991b4aca9605b83",
+    "url": "/GoogleBooks/static/css/main.8dedcd70.chunk.css"
+  },
+  {
+    "revision": "b9aac28d422c76e4d019",
+    "url": "/GoogleBooks/static/js/2.06ab2d90.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/GoogleBooks/static/js/2.06ab2d90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43451991b4aca9605b83",
+    "url": "/GoogleBooks/static/js/main.d429ab24.chunk.js"
+  },
+  {
+    "revision": "862ea229cef8f7cd02d0",
+    "url": "/GoogleBooks/static/js/runtime-main.71977f52.js"
+  }
+]);
